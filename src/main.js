e820abaee5a9bb1dd77085c3a89140df1969d053
Gary Gao <ggao@tableausoftware.com>
2015-10-07 18:48:42 -0700

    Refactor to add in more options at init

--- src/main.js
+++ src/main.js
@@ -27,31 +27,66 @@
     // Set plugin method name and defaults
     var pluginName = 'floatLabel',
         defaults = {
+          // In case you want to preserve labels as visible for no js, or old
+          // IE users.
+          wrapperInitClass: 'has-float-label',
+          // For a custom label selector, if you have multiple labels, for some
+          // reason.
+          labelSelector: false,
+          // Class given to label when its field has a non-null value. Toggled
+          // when the value is empty / falsy.
           activeClass: 'is-active',
+          // Class given to label when its field is focused. Toggled when it
+          // loses focus.
           focusClass: 'has-focus'
         };
 
     // plugin constructor
     function Plugin (element, options) {
       var $element = $(element);
-      this.element = element;
 
       // Set up internals for tracking, just in case.
-      this._defaults = defaults;
       this._name = pluginName;
+      this._defaults = defaults;
+      this._element = $element;
+
+      // Use the init options.
+      this.options = $.extend(true, defaults, options);
 
       // Set up a couple of internals to keep track of input and label.
-      this._input = _findInput($element);
-      this._label = _findLabel($element);
-
-      // Use the init options, and extend them over the defaults.
-      this.options = $.extend(true, defaults, options);
+      this._wrapper = $element;
+      this._input = this._findInput($element);
+      this._label = this._findLabel($element);
 
       // Do it now.
       this.init();
     }
 
-    //
+    // Utility: find a input that we want to alter the label for.
+    Plugin.prototype._findInput = function(el) {
+      var $textInputs = $(el).find('input, textarea').not('[type="checkbox"], [type="radio"]');
+      // The regular text input types.
+      if ($textInputs.length) {
+        return $textInputs;
+      }
+      // Try for select elements.
+      else {
+        return $(el).find('select');
+      }
+      return false;
+    };
+
+    // Utility: find a label in the field wrapper element.
+    Plugin.prototype._findLabel = function(el) {
+      // If a custom selector is provided
+      if (this.options.labelSelector) {
+        return $(el).find(this.options.labelSelector);
+      }
+
+      // Just try a label element.
+      return $(el).find('label');
+    };
+
     Plugin.prototype._onKeyUp = function (ev) {
       // On empty value, inactivate the label.
       if (this._input.val() === '') {
@@ -76,6 +111,10 @@
     };
 
     Plugin.prototype.init = function () {
+      // Mark the element as having been init'ed.
+      this._element.addClass(this.options.wrapperInitClass);
+
+      // Event bindings to the input element.
       this._input.on('keyup change', $.proxy(this._onKeyUp, this));
       this._input.on('blur', $.proxy(this._onBlur, this));
       this._input.on('focus', $.proxy(this._onFocus, this));
@@ -83,29 +122,13 @@
 
     // Lightweight constructor, preventing against multiple instantiations
     $.fn[pluginName] = function (options) {
-      return this.each(function () {
+      return this.each(function initPlugin() {
         if (!$.data(this, 'plugin_' + pluginName)) {
           $.data(this, 'plugin_' + pluginName,
           new Plugin(this, options));
         }
       });
     };
-
-    // Utility: find a input that we want to alter the label for.
-    function _findInput(el) {
-      if ($(el).find('input, textarea').length) {
-        return $(el).find('input, textarea');
-      }
-      else {
-        return $(el).find('select');
-      }
-      return false;
-    }
-
-    // Utility: find a input that we want to alter the label for.
-    function _findLabel(el) {
-      return $(el).find('label');
-    }
 
   });
 })(jQuery);
